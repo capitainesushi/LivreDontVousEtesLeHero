c1aecbba505a2aaa61e9ff5cb03ae7e4d7cc593e
Charles-Cooke <charles@mycourriel.ca>
2018-11-11 14:37:44 -0500

    Livre dont vous êtes le héro. Réalisé au début du programme (Février 2018)

---
+++ Livre_fonctions.h
@@ -0,0 +1,22 @@
+#ifndef Livre_fonctions_h
+	#define Livre_fonctions_h
+
+
+
+	namespace Livre_fonctions
+
+	{
+		void depart();
+		void troll();
+		void serpent();
+		void coffre();
+		void pieges();
+		void magicien();
+		void archer();
+		void porte();
+		void boss();
+		int dead();
+		int success();
+
+	}
+#endif  // Livre_fonctions.h
